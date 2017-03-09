0d376353cdd2cfda48f5b70215600b671123eb49
tfff1OFFICIAL <Maclean1>
2017-03-10 11:01:30 +1300

    did things

--- infininum/__init__.py
+++ infininum/__init__.py
@@ -9,24 +9,34 @@ TODO: implement multithreading in calculations?
 
 
 class InfiniNum:
-    def __init__(self, number="0"):
+    def __init__(self, number="0", log=False):
         self.num = []
+        self.log = log
         for char in str(number)[::-1]: # reversed so that looping from 0: will read in correct order
             self.num.append(char)
 
+    def _log(self, text):
+        """
+        Logs things
+        :param text: string, to print
+        :return: void
+        """
+        if self.log:
+            print(text)
+
     def plus(self, right_num):
-        print("Starting:\n for n in range(0, {})".format(len(right_num.num)))
+        self._log("Starting:\n for n in range(0, {})".format(len(right_num.num)))
         for n in range(0, len(right_num.num)):
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            self._log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
             # loop through the right Number
             try:
                 num = self.num[n]
                 # number does exist in num:
                 calc = int(num) + int(right_num[n])
-                print("Calculated {} + {} to be: {}".format(num, right_num[n], calc))
+                self._log("Calculated {} + {} to be: {}".format(num, right_num[n], calc))
                 if calc > 9:
-                    print("is > 9:\nInfininum({}{})".format(calc, "0"*(n-1)))
-                    # It's 2 digit. RIP, call self again...
+                    self._log("is > 9:\nInfininum({}{})".format(calc, "0"*(n-1)))
+                    # It's > 1 digit. RIP, call self again...
                     self.plus(InfiniNum("{}{}".format(calc, "0"*(n-1))))
                 else:
                     # It's 1 digit, we can handle that...
@@ -34,7 +44,7 @@ class InfiniNum:
             except:
                 # location doesn't exist in number
                 self.num.append(right_num[n])
-        print("Finished")
+        self._log("Finished")
 
     # Python built in things:
     def __getitem__(self, item):
@@ -44,3 +54,14 @@ class InfiniNum:
         :return: char, the character in that location
         """
         return self.num[item]
+
+    def __str__(self):
+        """
+        Print itself
+        :return: string, itself
+        """
+        to_out = ""
+        for i in self.num[::-1]:
+            to_out += i
+
+        return to_out
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from infininum import InfiniNum
 
-my_num = InfiniNum(28)
-my_num_2 = InfiniNum(13)
+my_num = InfiniNum(28, log=True)
+my_num_2 = InfiniNum(12)
 
 # should be 41
 my_num.plus(my_num_2)
 
-print(my_num.num)
+print(my_num)
